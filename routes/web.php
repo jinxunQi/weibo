1e1c03780b3bee3f52d9de42ee1d3edd6f3b6fea
574765035 <574765035@qq.com>
2019-08-24 14:49:52 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,13 @@ Route::delete('logout',"SessionsController@destroy")->name('logout');
 Route::get('/users/{user}/edit','UserController@edit')->name('users.edit');
 
 Route::get('signup/confirm/{token}','UserController@confirmEmail')->name('confirm_email');
+
+//--------密码重置----------------------------------------------------------------------------------------
+#---重置表单
+Route::get('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+#---发送邮件
+Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+#---密码更新页面
+Route::get('password/reset/{token}','Auth\ResetPasswordController@showResetForm')->name('password.reset');
+#---执行密码更新操作
+Route::post('password/reset','Auth\ResetPasswordController@reset')->name('password.update');
