789e8b2157e247e8c796eaa03a61cdf550ab557b
574765035 <574765035@qq.com>
2019-08-24 16:21:14 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,17 +2,31 @@
 @section('title','主页')
 
 @section('content')
-    <div class="jumbotron">
-        <h1>Hello laravel</h1>
-        <p class="lead">
-            你现在看到的是 <a href="http://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a>的示例项目主页
-        </p>
-        <p>
-            一切将从这里开始
-        </p>
-        <p>
-            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
-        </p>
-    </div>
-    <h1>主页</h1>
+    @if (Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info',['user'=>Auth::user()])
+                </section>
+            </aside>
+        </div>
+    @else
+        <div class="jumbotron">
+            <h1>Hello laravel</h1>
+            <p class="lead">
+                你现在看到的是 <a href="http://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a>的示例项目主页
+            </p>
+            <p>
+                一切将从这里开始
+            </p>
+            <p>
+                <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+            </p>
+        </div>
+    @endif
 @stop
